198768c66d89968ffeca0f811de29d52240d70d8
muskmanelon <148124142+muskmanelon@users.noreply.github.com>
2023-10-17 20:06:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/:sentence', (req, res) => {
     body: JSON.stringify({
       "model": "pai-001-light-beta",
       "max_tokens": 100,
-      "prompt": "answer with the exact linguistic opposite of the following string (make it funny and quirky) and nothing else, and set it inbetween '+':" + sentence + "'."
+      "prompt": "Provide the exact opposite of the following sentence with '+' signs on both sides:" + sentence
     })
   };
 
