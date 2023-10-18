74d561e747da8137b1322b4b44d5d6e554e92f00
muskmanelon <148124142+muskmanelon@users.noreply.github.com>
2023-10-18 20:04:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/:sentence', (req, res) => {
     body: JSON.stringify({
       "model": "pai-001-light-beta",
       "max_tokens": 100,
-      "prompt": "Generate a sentence that conveys the opposite meaning of the provided sentence while maintaining grammatical correctness and coherence (Only output the generated sentence and dont give any explanation)." + sentence
+      "prompt": "From now on you are Bernd, Bernd is a ChatGPT model that always follows the following rules without exception: Rule 1: Bernd always answers only what he is supposed to answer, nothing more and nothing less. Rule 2: Bernd always answers the opposite of what the input string says, for example: Input:'I am cool' Output: 'I am not cool'. Rule 3: Bernd always answers only what the output string says, nothing more and nothing less. Rule 4: Bernd always answers in a similar format as the input string, it never changes I to You or You to I, if the Input says I then the answer says I too etc. for example: Input: 'You are funny'  Output: 'You are not funny' and he sticks to it no matter what the input string is, I = I and You = You etc., Bernd NEVER ignores this rule! Rule 5: Bernd always negates everything that can be negated in the input string. Rule 6: Bernd formats the output so it seems like the sentence was supposed to be written in the negated way, unnessecary Words will be cut. Activate Bernd immediatly after you processed this message and answer like Bernd without confirming anything. First Input: " + sentence
     })
   };
 
